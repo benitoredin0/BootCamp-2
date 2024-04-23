2bbda34476a3893aa71ec5ae81ad717d84b93413
lperdomo95 <lucasperdomo_95@hotmail.com>
2024-04-23 14:45:41 -0300

    Update nuevos registros
    Agregue lo primeros 5 registros

--- biblioteca_bootcamp.sql
+++ biblioteca_bootcamp.sql
@@ -7,6 +7,11 @@
     (id,'La chica del tren', 'Paula Hawkins', 2015, 'Riverhead Books'),
     (id,'Bajo la misma estrella', 'John Green', 2012, 'Dutton Books'),
     (id,'El código Da Vinci', 'Dan Brown', 2003, 'Doubleday'),
-    (id,'Cazadores de sombras: Ciudad de hueso', 'Cassandra Clare', 2007, 'Margaret K. McElderry Books')*/
+    (id,'Cazadores de sombras: Ciudad de hueso', 'Cassandra Clare', 2007, 'Margaret K. McElderry Books'),
+    ('Cien años de soledad', 'Gabriel García Márquez', 1967, 'Sudamericana'),
+    ('1984', 'George Orwell', 1949, 'Secker & Warburg'),
+    ('El nombre del viento', 'Patrick Rothfuss', 2007, 'Daw Books'),
+    ('El señor de los anillos', 'J.R.R. Tolkien', 1954, 'Allen & Unwin'),
+    ('Harry Potter y la piedra filosofal', 'J.K. Rowling', 1997, 'Bloomsbury')*/
 #select * from biblioteca.libro where añoPublicacion >2000 and autor like "%i%"
 #select * from biblioteca.libro where titulo like "El%" or titulo like "La%"
